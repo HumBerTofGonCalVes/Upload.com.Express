ddfe03233079b539b6258506e8fe39dfa88ffcea
Humberto Gonçalves <humberto.c.goncalves@gmail.com>
2020-06-04 23:50:45 +0100

    Upload de arquivos com Node

---
+++ index.js
@@ -0,0 +1,32 @@
+/*jshint esversion: 6 */
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const path = require('path');
+
+app.set('view engine', 'ejs');
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, './uploads/');
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage
+});
+
+app.get('/', (req, res) => {
+    res.render('./index');
+});
+
+app.post('/upload', upload.single('arquivo'), (req, res) => {
+    res.send("Arquivo recebido!");
+});
+
+app.listen(8080, () => {
+    console.log("Servidor a trabalhar!");
+});
